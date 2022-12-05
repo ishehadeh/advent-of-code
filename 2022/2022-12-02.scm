edd387d342c01278f350118a29b8e912cf827d96
Ian Shehadeh <IanShehadeh2020@gmail.com>
2022-12-04 20:39:38 -0500

    day 2 part 2

--- 2022/2022-12-02.scm
+++ 2022/2022-12-02.scm
@@ -11,7 +11,13 @@
                            (opponent (car line-codepoints))
                            (response (car (cdr (cdr line-codepoints)))))
                         (decode-strategy-guide port (cons (cons (+ 1 (- opponent (char->integer #\A))) (+ 1 (- response (char->integer #\X)))) games))))))))
-    
+
+(define (wrap x min max)
+    (cond
+        ((< x min) (wrap (+ max (- x min -1)) min max))
+        ((> x max)  (wrap (+ min (- x max 1)) min max))
+        (else x)))
+
 (define (winner game)
     (match game
         ((3 . 1) 6)
@@ -20,13 +26,24 @@
         ((x . x) 3)
         (_ 0)))
 
+(define (choice-from-opponent-and-result game)
+    (match game
+        ((x . 2) x)
+        ((x . 1) (wrap (- x 1) 1 3))
+        ((x . 3) (wrap (+ x 1) 1 3))))
+
 (define (score-game game)
     (+ (cdr game) (winner game)))
 
+(define (score-from-opponent-and-result game)
+    (+  (choice-from-opponent-and-result game)
+        (* (- (cdr game) 1) 3)))
+
 (let* ((strategy-file (open-input-file "data/2.games.txt"))
        (strategy (decode-strategy-guide strategy-file))
-       (score (apply + (map score-game strategy))))
-    (display strategy)
-    (newline)
+       (score (apply + (map score-game strategy)))
+       (score-part-2 (apply + (map score-from-opponent-and-result strategy))))
     (display score)
+    (newline)
+    (display score-part-2)
     (newline))
