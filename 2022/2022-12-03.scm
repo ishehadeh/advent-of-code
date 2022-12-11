953f08c186f3a2cb669986319a77e5ba59fb1e31
Ian Shehadeh <IanShehadeh2020@gmail.com>
2022-12-11 10:23:21 -0500

    day 3: find and sum duplicates

--- 2022/2022-12-03.scm
+++ 2022/2022-12-03.scm
@@ -12,7 +12,12 @@
 (define (item->priority c)
     (cond
         ((char>=? c #\a) (+ 1 (- (char->integer c) (char->integer #\a))))
-        ((char>=? c #\A) (+ 26 (- (char->integer c) (char->integer #\A))))))
+        ((char>=? c #\A) (+ 27 (- (char->integer c) (char->integer #\A))))))
+
+(define (push-unique x lst)
+    (if (not (member x lst))
+        (append lst (list x))
+        lst))
 
 (define decode-rucksacks
     (case-lambda
@@ -24,7 +29,25 @@
                            (compartments (split all-items (/ (length all-items) 2))))
                         (decode-rucksacks port (append rucksacks (list compartments)))))))))
 
-(let* ((rucksacks-file (open-input-file "data/3.rucksacks.txt"))
-       (rucksacks (decode-rucksacks rucksacks-file)))
-    (display rucksacks)
-    (newline))
+(define duplicates
+    (case-lambda
+        ((list0 list1) (duplicates list0 list1 list1 '()))
+        ((list0 list1 list1-full dups)
+            (cond
+                ((eqv? list0 '()) dups)
+                ((eqv? list1 '()) (duplicates (cdr list0) list1-full list1-full dups))
+                ((eqv? (car list0) (car list1))
+                    (duplicates list0 (cdr list1) list1-full (push-unique (car list0) dups)))
+                (else
+                    (duplicates list0 (cdr list1) list1-full dups))))))
+
+(define (get-rucksacks)
+    (decode-rucksacks  (open-input-file "data/3.rucksacks.txt")))
+
+(define (main)
+    (let* ((rucksacks-file  (open-input-file "data/3.rucksacks.txt"))
+           (rucksacks       (decode-rucksacks rucksacks-file))
+           (dups            (map (lambda (x) (apply duplicates x)) (get-rucksacks)))
+           (priority-total  (apply + (map (lambda (x) (apply + x)) dups))))
+        (display priority-total)
+        (newline)))
