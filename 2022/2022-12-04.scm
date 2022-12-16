7da8b139ba3c1c5c43231c9b68a15cfb37e1e3b9
Ian Shehadeh <IanShehadeh2020@gmail.com>
2022-12-16 14:10:29 -0500

    day 4 part 2

--- 2022/2022-12-04.scm
+++ 2022/2022-12-04.scm
@@ -10,6 +10,17 @@
             (and (>= x1 x2) (<= y1 y2))
             (and (>= x2 x1) (<= y2 y1)))))
 
+(define (range-overlap-partial r1 r2)
+    (let ((x1 (car r1))
+        (y1 (cdr r1))
+        (x2 (car r2))
+        (y2 (cdr r2)))
+    (or
+        (and (>= x1 x2) (<= x1 y2))
+        (and (>= y1 x2) (<= y1 y2))
+        (and (>= x2 x1) (<= x2 y1))
+        (and (>= y2 x1) (<= y2 y1)))))
+
 (define string-split 
     (case-lambda
         ((s char_pred) (string-split s char_pred 0 '()))
@@ -32,19 +43,28 @@
                 (if (eof-object? line) sections
                     (decode-sections port (append sections (list (map decode-range (string-split line #\,))))))))))
 
-(define (count-section-overlaps sections)
-    (fold
-        (lambda (ranges n)
-            (if (range-overlap (car ranges) (cadr ranges))
-                (+ n 1)
-                n))
-        0
-        sections))
+(define (count-section-overlaps sections partial)
+    (let ((overlap-fn (if partial range-overlap-partial range-overlap)))
+        (fold
+            (lambda (ranges n)
+                (if (overlap-fn  (car ranges) (cadr ranges))
+                    (+ n 1)
+                    n))
+            0
+            sections)))
 
 (define (main-part1)
     (let* ((f        (open-input-file "./data/4.sections.txt"))
            (sections (decode-sections f))
-           (overlaps (count-section-overlaps sections)))
+           (overlaps (count-section-overlaps sections #f)))
+    (display "Overlaps: ")
+    (display overlaps)
+    (newline)))
+
+(define (main-part2)
+    (let* ((f        (open-input-file "./data/4.sections.txt"))
+           (sections (decode-sections f))
+           (overlaps (count-section-overlaps sections #t)))
     (display "Overlaps: ")
     (display overlaps)
     (newline)))
